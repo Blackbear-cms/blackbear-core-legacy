bf417129da5b04144440724f338db04dc9bc263a
endes123321 <endes123321@gmail.com>
2016-11-18 20:11:54 +0000

    Added dbmanagger for use all types of db drivers (sql and nosql)
    Signed-off-by: endes123321 <endes123321@gmail.com>

--- engines/core/db.php
+++ engines/core/db.php
@@ -8,49 +8,49 @@ if (!class_exists('db')) {
 
     function __construct() {
       global $fuel;
-      $settings = $fuel->read_file('dbmanager', 'json');
+      $settings = $fuel->read_file('config', 'json');
 
       if ($settings == null) {
-
+        $fuel->error('no config file or empty');
       } else {
         $this->dbmanager = $fuel->load_class($settings[0], $settings[1]);
       }
     }
 
     public function put_data($collection, $data) {
-      # code...
+      return $this->dbmanager->put_data($collection, $data);
     }
 
-    public function put_collect($collection, $data) {
-      # code...
+    public function put_various($collection, $data) {
+      return $this->dbmanager->put_various($collection, $data);
     }
 
-    public function get_all_data($collection) {
-      # code...
+    public function get_all_data($collect) {
+      return $this->dbmanager->get_all($collection);
     }
 
     public function create_collect($name, $structure) {
-      # code...
+      return $this->dbmanager->new_collect($name, $structure);
     }
 
     public function remove_collect($name) {
-      # code...
+      return $this->dbmanager->remove_collect($name);
     }
 
-    public function get_data($collection, $select, $where) {
-      # code...
+    public function get_data($collection, $where_and, $where_or) {
+      return $this->dbmanager->new_collect($collection, $where_and, $where_or);
     }
 
     public function update_data($collection, $where, $set) {
-      # code...
+      return $this->dbmanager->update($collection, $where, $set);
     }
 
     public function remove_data($collection, $where) {
-      # code...
+      return $this->dbmanager->remove($collection, $where);
     }
 
     public function is_sql() {
-      # code...
+      return $this->dbmanager->is_sql();
     }
   }
 
